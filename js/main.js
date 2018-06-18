c12ed10e14dd6d7d07735a30aaa4eef333ce1068
Confidence Okoghenun <confidenceboi@gmail.com>
2018-06-18 23:38:56 +0100

    Local Storage
    This commit includes changes that implement
    local storage api.
    
    *Function 'storeTaskInLocalStorage' was created
     to store todos.
    *This has made the inputed task persistent even
     after window refresh.
    *Next up is bug fixes and performance improvements.
    
    [closes-Bug: #006]

--- js/main.js
+++ js/main.js
@@ -21,36 +21,76 @@ function fadeOut(element) {
     if (element.style.opacity < 0.1) {
       clearInterval(fadeEffect);
       element.remove();
+
+      let todos = JSON.parse(localStorage.getItem("todo"));
+      todos.forEach((item, index) => {
+        if (element.textContent === item) {
+          todos.splice(index, 1);
+          localStorage.removeItem("todo");
+          localStorage.setItem("todo", JSON.stringify(todos));
+        }
+      });
     } else {
       element.style.opacity -= 0.1;
     }
   }, 50);
 }
 
-document
-  .querySelector("input[type=text]")
-  .addEventListener("keypress", function(event) {
-    if (event.which === 13) {
-      let todoText = this.value;
-      this.value = "";
-      let newTodo = document.createElement("li");
-      let newSpan = document.createElement("span");
-      newSpan.innerHTML = '<i class="fa fa-trash"></i>';
-      newTodo.innerHTML = todoText;
-      newTodo.insertAdjacentElement("afterbegin", newSpan);
-      document.querySelector("ul").appendChild(newTodo);
+if (localStorage.getItem("todo") === null) {
+  inputTask();
+} else {
+  JSON.parse(localStorage.getItem("todo")).forEach(item => {
+    createTaskItem(item);
+  });
+  inputTask();
+}
 
-      newTodo.addEventListener("click", function() {
-        this.classList.toggle("completed");
-      });
+function inputTask() {
+  document
+    .querySelector("input[type=text]")
+    .addEventListener("keypress", function(event) {
+      if (event.which === 13) {
+        let todoText = this.value;
+        this.value = "";
+        createTaskItem(todoText);
+        storeTaskInLocalStorage(todoText);
+      }
+    });
 
-      newSpan.addEventListener("click", function(event) {
-        fadeOut(this.parentNode);
-        event.stopPropagation();
-      });
-    }
+  document.querySelector(".fa-plus").addEventListener("click", function() {
+    document.querySelector("input[type=text]").classList.toggle("display");
+  });
+}
+
+function createTaskItem(item) {
+  let todoText = item;
+  let newTodo = document.createElement("li");
+  let newSpan = document.createElement("span");
+  newSpan.innerHTML = '<i class="fa fa-trash"></i>';
+  newTodo.innerHTML = todoText;
+  newTodo.insertAdjacentElement("afterbegin", newSpan);
+  document.querySelector("ul").appendChild(newTodo);
+
+  newTodo.addEventListener("click", function() {
+    this.classList.toggle("completed");
   });
 
-document.querySelector(".fa-plus").addEventListener("click", function() {
-  document.querySelector("input[type=text]").classList.toggle("display");
-});
+  newSpan.addEventListener("click", function(event) {
+    fadeOut(this.parentNode);
+    event.stopPropagation();
+  });
+  if (localStorage.getItem("todo") === null) {
+  }
+}
+
+function storeTaskInLocalStorage(todoText) {
+  let todo;
+  if (localStorage.getItem("todo") === null) {
+    todo = [];
+  } else {
+    todo = JSON.parse(localStorage.getItem("todo"));
+  }
+  todo.push(todoText);
+  localStorage.setItem("todo", JSON.stringify(todo));
+  console.log(todo);
+}
